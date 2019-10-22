5cdddc2da046ea05faec9c3136855fee716ac2ee
Ezzelyno Haggon <ehaggon@vo-d1.21-school.ru>
2019-10-22 14:24:30 +0300

    push

--- SONIA_PRINTF/ft_printf.c
+++ SONIA_PRINTF/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ehaggon <ehaggon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/21 16:31:33 by ehaggon           #+#    #+#             */
-/*   Updated: 2019/10/21 16:40:28 by ehaggon          ###   ########.fr       */
+/*   Updated: 2019/10/22 14:22:11 by ehaggon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,55 @@
 int				ft_printf(char *str, ...)
 {
 	va_list		ap;
-    int			res;
+    size_t		res;
     t_buf       buf;
     t_param     param;
 
     va_start(ap, str);
     ft_printf_init(&buf, &param);
-    res = buf.size;
+    res = ft_get_res(&buf, &param, &str, &ap);
     ft_print_res(&buf);
     ft_free_buf(&buf);
     va_end(ap);
-    return(res);
+    return((int)res);
 }
 
-int             ft_get_res()
+size_t          ft_get_res(t_buf *buf, t_param *param, char **str, va_list *ap)
 {
-    printf("%s\n", "получаю результат");;
-    return(0);
+    printf("%s\n", "получаю результат");
+    while (**str)
+	{
+		if (**str == '%')
+			ft_prcnt(buf, param, str, ap);
+		else if (**str == '{')
+			ft_col(str, buf);
+		else
+			ft_buf_add_c(&buf, *str);
+		*str ? str++ : NULL;
+		buf->err == 1 ? free(buf->buf) : 0;
+		if (buf->err == 1)
+			return (-1);
+	}
+    return(buf->size);
 }
 
 void            ft_print_res(t_buf *buf)
 {
     printf("%s\n","тут я печатаю результат");
     write(1, buf->buf, buf->top + 1);
+}
+
+void            ft_prcnt(t_buf *buf, t_param *param, char **str, va_list *ap)
+{
+    printf("%s\n", "тут стоит заглушка, которая обрабатывает процент");
+}
+
+void            ft_buf_add_c(t_buf *buf, char **str)
+{
+    printf("%s\n", "тут стоит заглушка, которая обрабатывает процент");
+}
+
+void            ft_col(t_buf *buf,char **str)
+{
+    printf("%s\n", "тут стоит заглушка, которая называется ft_col");
 }
--- SONIA_PRINTF/ft_printf.h
+++ SONIA_PRINTF/ft_printf.h
@@ -25,7 +25,7 @@ typedef struct		s_param
 
 # define BUF_SIZE 1000
 int				ft_printf(char *str, ...);
-int             ft_get_res();
+size_t          ft_get_res(t_buf *buf, t_param *param, char **str, va_list *ap);
 void            ft_buf_init(t_buf *buf);
 char			*ft_strnew(size_t size);
 void            *ft_memset(void *b, int c, size_t len);
@@ -33,3 +33,6 @@ void            ft_printf_init(t_buf *buf, t_param *param);
 void            ft_param_init(t_param *param);
 void			ft_free_buf(t_buf *buf);
 void            ft_print_res(t_buf *buf);
+void            ft_prcnt(t_buf *buf, t_param *param, char **str, va_list *ap);
+void            ft_buf_add_c(t_buf *buf, char **str);
+void            ft_col(t_buf *buf,char **str);
