465082fbe98275bb00a0e927ecd8a5305e099527
Ezzelyno Haggon <ehaggon@ga-c5.21-school.ru>
2019-10-22 20:40:48 +0300

    WHY TANYA WHY?

--- SONIA_PRINTF/types/ft_type_d.c
+++ SONIA_PRINTF/types/ft_type_d.c
@@ -1,6 +1,6 @@
 #include "ft_printf.h"
 
-void		ft_type_d(t_param param, va_list ap, t_buf *buf)
+void		ft_type_d(t_param param, va_list *ap, t_buf *buf)
 {
 			printf("%s\n", "		Обрабатываем, флаг d");
 }
